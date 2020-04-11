200bb4a7afd3158109359dba1a3b787ec52d9f97
marcosmilitao <marcosxr7@gmail.com>
2020-04-11 17:26:24 -0300

    Retrofit config

--- app/src/main/java/com/militao/herois/MainActivity.java
+++ app/src/main/java/com/militao/herois/MainActivity.java
@@ -1,8 +1,19 @@
 package com.militao.herois;
 
 import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.militao.herois.api.Api;
+import com.militao.herois.model.Personagem;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Api.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        Api api = retrofit.create(Api.class);
+
+
+        Call<List<Personagem>> call = api.listarPersonagens();
+
+        call.enqueue(new Callback<List<Personagem>>() {
+            @Override
+            public void onResponse(Call<List<Personagem>> call, Response<List<Personagem>> response) {
+                Log.d("OK", "><><><><><><><>><<><><>><><><>><><><><><><");
+                List<Personagem> lista = response.body();
+
+                for(Personagem p : lista ){
+                    Log.d("Nome",p.getName());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Personagem>> call, Throwable t) {
+                Log.d("ERROR", "><><><><><><><>><<><><>><><><>><><><><><><");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/militao/herois/api/Api.java
@@ -0,0 +1,15 @@
+package com.militao.herois.api;
+
+import com.militao.herois.model.Personagem;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface Api {
+    String BASE_URL = "http://192.168.0.22:3000/";
+
+    @GET("people")
+    Call<List<Personagem>> listarPersonagens();
+}
